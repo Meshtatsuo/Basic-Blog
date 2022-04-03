1a05a3b30a36234482cbddebdc15243643a6bb86
meshtatsuo <meshtatsuo@gmail.com>
2022-04-03 11:49:31 -0600

    force resync heroku for init deployment

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ app.use(routes);
 
 // turn on connection to db and server
 sequelize.sync({
-    force: false
+    force: true
 }).then(() => {
     app.listen(PORT, () => console.log('Now listening'));
 });
